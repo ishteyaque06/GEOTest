20aace9432baf04f167088b7beb6dda6c7209248
ishteyaque06 <ishteyaque.ahmad@cyient.com>
2017-12-13 20:53:01 +0530

    Initial Commit

---
+++ AdapptTestGoe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdapptTestGoe
+//
+//  Created by Ahmed on 13/12/17.
+//  Copyright © 2017 Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
